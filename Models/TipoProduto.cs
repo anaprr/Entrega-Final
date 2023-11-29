0517b896510dcc7b25c0e95ef1f53c385ff9bf28
anaprr <anapaularamos20101@hotmail.com>
2023-11-29 16:46:23 -0300

    .

--- Models/TipoProduto.cs
+++ Models/TipoProduto.cs
@@ -1,5 +1,8 @@
-﻿namespace Projeto_Final.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_Final.Models
 {
+    [Table("TipoProduto")]
     public class TipoProduto
     {
     }
